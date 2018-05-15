5340ebcaeebbf98e724ce6731838b9c962faf8f0
huangzehai <chaogeek@foxmail.com>
2018-05-15 13:13:07 +0800

    Job扩展自Configured

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # hadoop-example
 Hadoop map reduce example
 
-
+hdfs dfs -rm -r /user/huangzehai/wordcount/output
 hadoop jar target/hadoop-example-1.0-SNAPSHOT.jar huangzehai.mr.wordcout.WordCount /user/huangzehai/wordcount/input /user/huangzehai/wordcount/output
--- src/main/java/huangzehai/mr/wordcout/WordCount.java
+++ src/main/java/huangzehai/mr/wordcout/WordCount.java
@@ -1,6 +1,7 @@
 package huangzehai.mr.wordcout;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -9,11 +10,30 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class WordCount {
+public class WordCount extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+        conf.set("mapreduce.job.maps", "3");
+        conf.set("mapreduce.job.reduces", "2");
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
 
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
@@ -32,7 +52,7 @@ public class WordCount {
     }
 
     public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values,
@@ -48,16 +68,7 @@ public class WordCount {
     }
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        int exitCode = ToolRunner.run(new WordCount(), args);
+        System.exit(exitCode);
     }
 }
